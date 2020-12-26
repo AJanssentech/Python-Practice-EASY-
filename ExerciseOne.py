c4bb65411d67bbde42cdf2b4426ded7c1b08a0d7
AJanssentech <74354999+AJanssentech@users.noreply.github.com>
2020-12-27 00:31:19 +0100

    Add files via upload

---
+++ ExerciseOne.py
@@ -0,0 +1,11 @@
+number =0
+
+def main () : 
+    print("Welkom,")
+    name = input("What is your name ?\n")
+    age = input("What is your age asswell?\n")
+    print ("Your name is " + name)
+    number = 100 - int(age)
+    print("You'll be 100 in " + str(number) + " years.")
+
+main()    
