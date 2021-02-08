2af561154ed9dc5f59353854e0373b1f04877093
AJanssentech <74354999+AJanssentech@users.noreply.github.com>
2021-02-08 19:09:59 +0100

    Add files via upload

--- ExerciseTree.py
+++ ExerciseTree.py
@@ -1,18 +1,28 @@
+listOne = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
 def main() : 
-    listOne = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
     for x in listOne : 
         if x <= 5 : 
-            #print(x)
-            #Extra's 
-            extraList = []
-            extraList.append(x)
-            print(extraList)
+            print(x)
+
+def extraOne() : 
+    extralist = []   
+    for i in listOne : 
+        if i <= 5 :
+            extralist.append(i)
+    print(extralist)
+
+def extraTwo() :
+    extralist =[]
     look = input("Please enter a number\nWe'll look if there any numer smaller in the list\n")
     print("The numbers that is smaller than your nummer")
-    for i in listOne : 
-        if int(look) >= int(i) :
-            print(i)      
+    for j in listOne :
+        if (int(look) > j) : 
+            extralist.append(j)
+    print(extralist)
+                 
+main()
+extraOne()
+extraTwo()
 
-
-
-main()        
+    
